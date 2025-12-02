dd86602cc5d97f217c60f880589498bcc321818b
Kenai2 <kenrivmul@alu.edu.gva.es>
2025-12-02 13:50:03 +0100

    si

--- UD6/src/main/java/Ej_UD6_1C/Ej8.java
+++ UD6/src/main/java/Ej_UD6_1C/Ej8.java
@@ -1,6 +1,6 @@
 package Ej_UD6_1C;
 
-import java.util.Scanner;
+
 
 public class Ej8 {
     public static void main(String[] args) {
@@ -10,9 +10,10 @@ public class Ej8 {
         final int TAMANHOMAX = 210;
         final int TAMANHOMIN = 140;
 
-        int media = 0;
-        int numMaximo = Integer.MAX_VALUE;
-        int numMinimo = Integer.MIN_VALUE;
+        int media [] = new int[TAMANHO1];
+        int maximo [] = new int[TAMANHO1];
+        int minimo [] = new int[TAMANHO1];
+
 
         String nombrePais [] = {"España","Alemania","Francia","Italia"};
 
@@ -20,10 +21,44 @@ public class Ej8 {
         int matriz [][] = new int[TAMANHO1][TAMANHO2];
 
         for (int i = 0; i<TAMANHO1;i++){
+            int suma = 0;
+            int max = 0;
+            int min = TAMANHOMAX;
             for (int j = 0; j<TAMANHO2;j++){
                 matriz[i][j] = (int)(Math.random() * (TAMANHOMAX-TAMANHOMIN+1) + TAMANHOMIN);
-                System.out.println(matriz[i][j]);
+                //System.out.println(matriz[i][j]);
+                suma += matriz[i][j];
+                if(max < matriz[i][j]){
+                    max = matriz[i][j];
+                }
+                if(min > matriz[i][j]){
+                    min = matriz[i][j];
+                }
             }
+            if (i == 0){
+                media [i] = suma / TAMANHO2;
+            }
+            else if (i == 1){
+                media [i] = suma / TAMANHO2;
+            }
+            else if (i == 2){
+                media [i] = suma / TAMANHO2;
+            }
+            else if (i == 3){
+                media [i] = suma / TAMANHO2;
+            }
+            maximo[i] = max;
+            minimo[i] = min;
         }
+        for (int i = 0; i<12;i++){
+            System.out.print("\t");
+        }
+        System.out.print("MED MIN MAX" +"\n");
+
+
+        /*System.out.println(nombrePais [0] +": " + + media [0] +" " +maximo [0] +" " +minimo [0]);
+        System.out.println(nombrePais [1] +": " + media [1] +" " +maximo [1] +" "  +minimo [1]);
+        System.out.println(nombrePais [2] +": " + media [2] +" "  +maximo [2] +" "  +minimo [2]);
+        System.out.println(nombrePais [3] +": " + media [3] +" "  +maximo [3] +" "   +minimo [3]);*/
     }
 }
